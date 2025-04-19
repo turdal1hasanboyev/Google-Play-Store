4d45be24d6ab7052c9d1bf664c822b2afd0e9ec3
Turdali <turdalihasanboyev70@gmail.com>
2025-04-19 21:16:39 +0500

    `Refactor admin.py files for Review and CustomUser models`

--- apps/review/admin.py
+++ apps/review/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 from .models import Review
 
 
-admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'app',
+        'user',
+        'rating',
+        'is_active',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = ('is_active',)
--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+
 from .models import CustomUser
 
 
@@ -8,4 +10,83 @@ admin.site.site_title = "Google Play Store Admin Panel"
 admin.site.index_title = "Welcome to Google Play Store Admin Panel!"
 
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom User Admin Model
+    """
+
+    model = CustomUser # model
+
+    ordering = ('email',) # tartiblash
+
+    search_fields = ( # qidiruv uchun
+        'email',
+        'phone_number',
+        'first_name',
+        'last_name',
+    )
+
+    list_display = ( # displayda ko'rinish uchun
+        'id',
+        'get_full_name',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'image',
+        'is_active',
+        'is_superuser',
+        'is_staff',
+        'last_login',
+        'date_joined',
+    )
+
+    list_filter = ( # filter uchun
+        'is_active',
+        'is_superuser',
+        'is_staff',
+    )
+
+    readonly_fields = ( # readonly uchun
+        'id',
+        'last_login',
+        'date_joined',
+    )
+
+    # prepopulated_fields = { # avto to'ldirish uchun
+    #     'slug': ('first_name', 'last_name'),
+    # }
+
+    fieldsets = ( # bezak uchun
+        ('Login', {
+            'fields': ('email', 'password',),
+            'classes': ('wide',), # wide katta korinish uchun
+        }),
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'phone_number', 'image', 'bio',),
+            'classes': ('wide',),
+        }),
+        ("Permissions", {
+            'fields': ('is_superuser', 'is_staff', 'is_active',),
+            'classes': ('wide',),
+        }),
+        ("Important Dates", {
+            'fields': ('date_joined', 'last_login',),
+            'classes': ('wide',),
+        }),
+        ("ID", {
+            'fields': ('id',), # bu uchun readonly bolishi kerak
+            'classes': ('wide',),
+        }),
+    )
+    add_fieldsets = ( # superuser uchun
+        ('Create Super User', {
+            'fields': ('email', 'password1', 'password2',),
+            'classes': ('wide',),
+        }),
+        ("Permissions", {
+            'fields': ('is_superuser', 'is_staff',),
+            'classes': ('wide',),
+        }),
+    )
