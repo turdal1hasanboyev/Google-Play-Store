6e2968c514c747fee9f79dbc5b0f8d1311b2a64c
Turdali <turdalihasanboyev70@gmail.com>
2025-04-19 19:37:53 +0500

    "Refactor admin registration for Category model to use @admin.register decorator and define CategoryAdmin class."

--- apps/category/admin.py
+++ apps/category/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from .models import Category
 
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'is_active',
+        'created_at',
+        'updated_at',
+    )
+    search_fields = ('name',)
+    list_filter = ('is_active',)
